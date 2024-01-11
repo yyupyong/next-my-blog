3746071726395e3379043c84de63ddb524ea7ad1
kim <yupyong.kim@gmail.com>
2024-01-11 23:59:59 +0900

    コメント

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return (
-        <footer className="my-16 flex flex-col justify-center w-6/6 sm:w-[610px] md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px] p-6 mx-auto  border-t-2 border-grey-100" >
-            <span className="text-center text-base"> Copyright by <a href="https://github.com/frontendweb3"> Frontend web </a> </span>
-        </footer>
-    );
+  return (
+    <footer className="w-6/6 border-grey-100 my-16 mx-auto flex flex-col justify-center border-t-2 p-6 sm:w-[610px] md:w-[610px] lg:w-[610px]  xl:w-[610px] 2xl:w-[610px]">
+      <span className="text-center text-base">
+        {" "}
+        Copyright by <a href="https://github.com/frontendweb3">
+          {" "}
+          YuuCoading{" "}
+        </a>{" "}
+      </span>
+    </footer>
+  );
 }
 
 export default Footer;

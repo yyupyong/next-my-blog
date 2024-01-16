f699aa65ed79616ece887bdfa3d647249763fd5e
kim <yupyong.kim@gmail.com>
2024-01-16 23:59:59 +0900

    fix

--- pages/tag/[tag].tsx
+++ pages/tag/[tag].tsx
@@ -1,34 +1,28 @@
-import React from 'react';
-import Header from '../../components/Header';
-import Post from '../../components/Post';
-import Newsletter from '../../components/Newsletter';
-import { posts as data } from '../../data/post';
+import React from "react";
+import Header from "../../components/Header";
+import Post from "../../components/Post";
+import Newsletter from "../../components/Newsletter";
+import { posts as data } from "../../data/post";
 import dayjs from "dayjs";
 
 function Tag({ posts, tag }: tagPageProps) {
-
   return (
-
     <>
-      <Header title='hidden' tag={`${tag}`} />
+      <Header title="hidden" tag={`${tag}`} />
 
-      <main className='container mx-auto flex flex-col p-3'>
-
-        {
-          posts.map(
-            (item: itemProps) => {
-
-              return <Post key={item.id}
-                tag={item.tags[0]}
-                date={dayjs(item.date).format("DD MMMM , YYYY")}
-                title={item.title}
-                description={item.description}
-                image={item.image}
-              />
-            }
-          )
-        }
-
+      <main className="container mx-auto flex flex-col p-3">
+        {posts.map((item: itemProps) => {
+          return (
+            <Post
+              key={item.id}
+              tag={item.tags[0]}
+              date={dayjs(item.date).format("DD MMMM , YYYY")}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+            />
+          );
+        })}
       </main>
 
       <Newsletter />
@@ -49,9 +43,8 @@ interface tagPageProps {
     author: string;
     category: string[];
     id: string;
-  }
+  };
 }
-
 
 interface itemProps {
   date: string;
@@ -65,26 +58,27 @@ interface itemProps {
 }
 
 export async function getStaticPaths() {
-
   const paths = data.map((item) => ({
     params: { tag: item.category[0].toLowerCase().replaceAll(" ", "-") },
-  }))
-
+  }));
 
   return {
     paths: paths,
     fallback: false,
-  }
+  };
 }
 
-export async function getStaticProps(context: { params: { tag: string; }; }) {
+export async function getStaticProps(context: { params: { tag: string } }) {
+  const {
+    params: { tag },
+  } = context;
 
-  const { params: { tag } } = context
-
-  const posts = data.filter((item) => item.category[0].toLowerCase().replaceAll(" ", "-") === tag)
+  const posts = data.filter(
+    (item) => item.category[0].toLowerCase().replaceAll(" ", "-") === tag
+  );
 
   return {
     props: { posts, tag },
-  }
+  };
 }
 export default Tag;

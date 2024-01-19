677c1a4c72afe715ff7494a13ae9df142880d178
kim <yupyong.kim@gmail.com>
2024-01-19 23:59:59 +0900

    import client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Header from "../components/Header"
-import Post from '../components/Post';
-import Newsletter from '../components/Newsletter';
-import Pagination from '../components/Pagination';
-import {posts as data} from '../data/post';
+import Header from "../components/Header";
+import Post from "../components/Post";
+import Newsletter from "../components/Newsletter";
+import Pagination from "../components/Pagination";
+import { posts as data } from "../data/post";
 import dayjs from "dayjs";
+import { client } from "../libs/client";
 
 // sm: md: lg: xl: 2xl:
 /*
@@ -16,52 +17,42 @@ xl	1280px	@media (min-width: 1280px) { ... }
 */
 
 export default function Home({ posts }: homePageProps) {
-
-  
-
   return (
     <>
       <Header />
 
-      <main className='container mx-auto flex flex-col p-3'>
-
-        <p className="text-4xl inline font-bold text-left ml-0  sm:ml-0 md:ml-10 lg:ml-10 xl:ml-10 2xl:ml-24  my-16 leading-[normal] text-[rgba(35,46,82,1)]">
+      <main className="container mx-auto flex flex-col p-3">
+        <p className="my-16 ml-0 inline text-left text-4xl  font-bold leading-[normal] text-[rgba(35,46,82,1)] sm:ml-0 md:ml-10  lg:ml-10 xl:ml-10 2xl:ml-24">
           All posts
         </p>
 
-        {
-          posts?.map(
-            (item: itemProps) => {
-              let GetDate = dayjs(item.date).format("DD-MMM , YYYY")
+        {posts?.map((item: itemProps) => {
+          let GetDate = dayjs(item.date).format("DD-MMM , YYYY");
 
-              return <Post key={item.id}
-                tag={item.tags[0]}
-                date={GetDate.toString()}
-                title={item.title}
-                description={item.description}
-                image={item.image}
-              />
-            }
-          )
-        }
+          return (
+            <Post
+              key={item.id}
+              tag={item.tags[0]}
+              date={GetDate.toString()}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+            />
+          );
+        })}
 
         <Pagination />
-
-
       </main>
-
 
       <Newsletter />
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
-
   return {
-    props: { posts:data },
-  }
-
+    props: { posts: data },
+  };
 }
 
 interface homePageProps {
@@ -75,7 +66,7 @@ interface homePageProps {
     author: string;
     category: string[];
     id: string;
-  }
+  };
 }
 
 interface itemProps {
@@ -87,5 +78,4 @@ interface itemProps {
   author: string;
   category: string[];
   id: string;
-
 }

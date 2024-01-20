3b8b98d4cfd6cf8dc7d2280acac0f9adc674d1b4
kim <yupyong.kim@gmail.com>
2024-01-20 23:59:59 +0900

    delete comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,6 @@ import { posts as data } from "../data/post";
 import dayjs from "dayjs";
 import { client } from "../libs/client";
 
-// sm: md: lg: xl: 2xl:
-/*
-sm	640px	@media (min-width: 640px) { ... }
-md	768px	@media (min-width: 768px) { ... }
-lg	1024px	@media (min-width: 1024px) { ... }
-xl	1280px	@media (min-width: 1280px) { ... }
-2xl	1536px	@media (min-width: 1536px) { ... }
-
-*/
-
 export default function Home({ posts }: homePageProps) {
   return (
     <>
